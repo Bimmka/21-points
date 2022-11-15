85ad17e934026a550b670c0cd71536d2890f7128
Bimmka <bibimmka@gmail.com>
2022-11-15 19:31:18 +0100

    Base game bootstrapp

---
+++ Assets/Features/Bootstrapp/Scripts/GameSceneBootstrapper.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Features.Bootstrapp.Scripts
+{
+  public class GameSceneBootstrapper : MonoInstaller
+  {
+    public override void InstallBindings()
+    {
+      
+    }
+  }
+}
---
+++ Assets/Features/Bootstrapp/Scripts/ProjectBootstrapper.cs
@@ -0,0 +1,56 @@
+﻿using Features.GameStates;
+using Features.GameStates.States;
+using Features.GameStates.States.Interfaces;
+using UnityEngine;
+using Zenject;
+
+namespace Features.Bootstrapp.Scripts
+{
+  public class ProjectBootstrapper : MonoInstaller
+  {
+    [SerializeField] private GameObserver gameObserverPrefab;
+
+    public override void Start()
+    {
+      base.Start();
+      ResolveGameStates();
+      GameObject gameObserver = SpawnGameObserver();
+      StartGame(gameObserver);
+    }
+
+    public override void InstallBindings()
+    {
+      BindGameStateMachine();
+      BindGameStates();
+      BindGame();
+    }
+
+    private void ResolveGameStates()
+    {
+      Container.Resolve<GameFinishState>();
+      Container.Resolve<GameLoadState>();
+      Container.Resolve<GameLoopState>();
+      Container.Resolve<MainMenuState>();
+    }
+
+    private GameObject SpawnGameObserver() => 
+      Container.InstantiatePrefab(gameObserverPrefab);
+
+    private void StartGame(GameObject gameObserver) => 
+      gameObserver.GetComponent<GameObserver>().StartGame();
+
+    private void BindGameStateMachine() => 
+      Container.Bind<IGameStateMachine>().To<GameStateMachine>().FromNew().AsSingle();
+
+    private void BindGameStates()
+    {
+      Container.Bind<GameFinishState>().ToSelf().FromNew().AsSingle();
+      Container.Bind<GameLoadState>().ToSelf().FromNew().AsSingle();
+      Container.Bind<GameLoopState>().ToSelf().FromNew().AsSingle();
+      Container.Bind<MainMenuState>().ToSelf().FromNew().AsSingle();
+    }
+
+    private void BindGame() => 
+      Container.Bind<Game>().ToSelf().FromNew().AsSingle();
+  }
+}
